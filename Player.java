caa194269f184315a838919049ed59ea385e3a10
JoshIsCodingRN <132061175+JoshIsCodingRN@users.noreply.github.com>
2024-03-23 23:16:49 -0400

    it compiles rn

--- Player.java
+++ Player.java
@@ -1,53 +1,58 @@
+import java.util.Scanner;
 
-public class Player
-{
+public class Player {
     private int monkeyID;
     private int wager;
     private int wallet;
+    private int buyIn;
+    Scanner wagerChoice;
 
-
-    public Player(int monkeyID, int wager, int wallet)
+    public Player(int monkeyID, int wager, int wallet) 
     {
         this.monkeyID = monkeyID;
         this.wager = wager;
         this.wallet = wallet;
+        this.wagerChoice = new Scanner(System.in);
     }
 
-
-    public void setChoice(int monkeyID)
+    public void setChoice(int monkeyID) 
     {
         this.monkeyID = monkeyID;
     }
 
-
-    public int getChoice()
+    public int getChoice() 
     {
         return this.monkeyID;
     }
 
-
     public void setWager(int wager)
     {
-        //TODO: no wager over amount of money
         this.wager = wager;
     }
 
-
-    public int getWager()
+    public int getWager() 
     {
         return this.wager;
     }
 
-
-    public void setMoney(int wallet)
+    public void setMoney(int wallet) 
     {
         this.wallet = wallet;
     }
 
-
-    public int getMoney()
+    public int getMoney() 
     {
         return this.wallet;
     }
-    
+
+    public void setBuyIn(int buyIn)
+    {
+        this.buyIn = buyIn;
+    }
+
+    public int getBuyIn()
+    {
+        return buyIn;
+    }
+
 }
