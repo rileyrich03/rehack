800f01cb984dc6243f793b2d02a33685bf143a7f
rileyrich03 <rileyrichardson0703@gmail.com>
2024-03-23 22:31:16 -0400

    actually saved

---
+++ Driver.java
@@ -0,0 +1,45 @@
+public class Driver
+{
+	private Monkey[] table;
+	private Player player;
+	String word;
+	public Driver() 
+	{
+
+	}
+	
+	public static void Main(String[] args)
+	{
+
+	}
+
+	public void StartGame()
+	{
+
+	}
+
+	public bool checkEndGame()
+	{
+		return false;
+	}
+
+	public bool checkEndRound()
+	{
+		return false;
+	}
+
+	public bool endGame()
+	{
+		return false;
+	}
+
+	public bool endRound()
+	{
+		return false;
+	}
+
+	public void setWord(String word)
+	{
+
+	}
+}
--- Monkey.java
+++ Monkey.java
@@ -1,26 +1,37 @@
+import java.util.Random;
+
 public class Monkey{
 	private String name;
-	private String word;
+	private Char[] word;
 	private int monkeyID;
 	private int correct;
 	public Monkey()
 	{
-
+		this.name = null;
+		this.monkeyID = 0;
+		this.correct = 0;
+		this.word = newChar[0];
 	}
-	public Monkey(String name, int monkeyID)
+	public Monkey(String name, int monkeyID, int wordLength)
 	{
 		this.name = name;
 		this.monkeyID = monkeyID;
 		this.correct = 0;
+		this.word = new Char[wordLength];
 	}
 
-	private char chooseLetter()
+	private char[] chooseWord()
 	{
+		Random rand = new Random();
+		int numLetters = this.word.length();
+		for(int i = 0; i < numletters; i++)
+			word[i] = 65 + rand.next() % 26;
+	
 		return 0;
 	}
-	public lookWord(String word)
+	public String makeWord()
 	{
-
+		return null;
 	}
 	public int getCorrect()
 	{
@@ -28,6 +39,14 @@ public class Monkey{
 	}
 	public void setCorrect(int correct)
 	{
-		
+		this.correct = correct;
+	}
+	public String getWord()
+	{
+		return this.word;
+	}
+	public void setWord(String word)
+	{
+		this.word = word;
 	}
 }
